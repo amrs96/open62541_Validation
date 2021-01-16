fbd7828817ced8e440084f2d2a6d40ea9946a4ff
aboahmad <ehab.alrahhal@mni.thm.de>
2021-01-16 14:18:41 +0100

    Create a new file Example_ClientFilter_Events

---
+++ examples/Example_ClientFilter_Events.c
@@ -0,0 +1,16 @@
+/**
+ * In this C file we will create a ClientFilter example.
+ * Client configured on the server
+ * */
+
+
+#include <open62541/client.h>
+#include <open62541/client_config_default.h>
+#include <open62541/client_highlevel.h>
+#include <open62541/client_subscriptions.h>
+#include <open62541/plugin/log_stdout.h>
+#include <open62541/server.h>
+#include <open62541/server_config_default.h>
+#include <open62541/util.h>
+
+#include <signal.h>
